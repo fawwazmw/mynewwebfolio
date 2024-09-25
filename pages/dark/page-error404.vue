f28b2754409a8b78b4eb6bcc70d895fa926570c2
fawwazmw <mahesfawwaz79@gmail.com>
2024-09-25 15:12:48 +0700

    change README 3

--- pages/dark/page-error404.vue
+++ pages/dark/page-error404.vue
@@ -14,20 +14,20 @@
   </div>
 </template>
 <script setup>
-import Footer from '@/components/dark/modern-agency/Footer';
-import Lines from '@/components/dark/common/Lines';
-import ProgressScroll from '@/components/dark/common/ProgressScroll';
-import Cursor from '@/components/dark/common/cusor';
-import LoadingScreen from '@/components/dark/common/loader';
-import Navbar from '@/components/dark/creative-agency/Navbar';
-import Error from '@/components/dark/error404/Error';
-import { onMounted } from 'vue';
+import Footer from "~/components/dark/modern-agency/Footer";
+import Lines from "@/components/dark/common/Lines";
+import ProgressScroll from "@/components/dark/common/ProgressScroll";
+import Cursor from "@/components/dark/common/cusor";
+import LoadingScreen from "@/components/dark/common/loader";
+import Navbar from "@/components/dark/creative-agency/Navbar";
+import Error from "@/components/dark/error404/Error";
+import { onMounted } from "vue";
 
 useHead({
   link: [
-    { rel: 'stylesheet', href: '/dark/assets/css/plugins.css' },
-    { rel: 'stylesheet', href: '/dark/assets/css/satoshi.css' },
-    { rel: 'stylesheet', href: '/dark/assets/css/style.css' },
+    { rel: "stylesheet", href: "/dark/assets/css/plugins.css" },
+    { rel: "stylesheet", href: "/dark/assets/css/satoshi.css" },
+    { rel: "stylesheet", href: "/dark/assets/css/style.css" },
   ],
   // script: [{ src: '/dark/assets/js/smoother-script.js', defer: true }],
 });
